4d3e126cbfb9411cc9ba93e431a5beb82e1880db
Aaditya Sharma <aadityasharma00445@gmail.com>
2024-01-24 22:40:44 +0530

    first line of code

---
+++ Day_1.cpp
@@ -0,0 +1,10 @@
+//  24/1/24 22:40 
+//This is the first day of learnign dsa
+// Today we write first line of codes
+
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Namaste ji";
+}
